718602cc8c3d9b8e1727812192dfce5f4356f595
Estefania M <estefania08@gmail.com>
2025-05-01 19:10:08 -0500

    prueba render

--- src/main/java/com/innovatech/solution/nomina/NominaApplication.java
+++ src/main/java/com/innovatech/solution/nomina/NominaApplication.java
@@ -1,14 +1,31 @@
 package com.innovatech.solution.nomina;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication()
 public class NominaApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(NominaApplication.class, args);
-	}
+
+	public static void main(String[] args)
+
+		{
+			Dotenv dotenv = Dotenv.configure()
+					.directory("./")
+					.ignoreIfMalformed()
+					.ignoreIfMissing()
+					.load();
+
+			dotenv.entries().forEach(entry -> {
+				System.setProperty(entry.getKey(), entry.getValue());
+			});
+
+			SpringApplication.run(NominaApplication.class, args);
+		}
+
+
 }
